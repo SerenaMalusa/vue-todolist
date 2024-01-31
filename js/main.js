da74094370f2477c0c0222b90981adad93195ab6
SerenaMalusa <sere.malu@gmail.com>
2024-01-31 16:59:38 +0100

    feat: create all tasks with Vue

--- js/main.js
+++ js/main.js
@@ -3,7 +3,32 @@ const { createApp } = Vue;
 const app = createApp ({
     data() {
         return {
-
+            tasks: [
+                {
+                    text: 'cosa da fare 1',
+                    done: false
+                },
+                {
+                    text: 'cosa da fare 2',
+                    done: false
+                },
+                {
+                    text: 'cosa da fare 3',
+                    done: false
+                },
+                {
+                    text: 'cosa da fare 4',
+                    done: false
+                },
+                {
+                    text: 'cosa da fare 5',
+                    done: false
+                },
+                {
+                    text: 'cosa da fare 6',
+                    done: false
+                }
+            ],
         }
     },
     methods: {
